84397863b5cceea003762d29c77595e11252848b
Ahsanul Azim Plabon <ahsanulazim6@gmail.com>
2025-09-28 21:06:47 +0600

    env connected

--- config/database.js
+++ config/database.js
@@ -1,8 +1,14 @@
 import { MongoClient, ServerApiVersion } from "mongodb";
+import dotenv from "dotenv";
+dotenv.config();
 
 const uri = process.env.MONGODB_URI;
 const client = new MongoClient(uri, {
-    serverApi: { version: ServerApiVersion.v1, strict: true, deprecationErrors: true },
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
 });
 
 await client.connect();
