d989d96080eb6c97186d835a69689015432967fb
Ahsanul Azim Plabon <ahsanulazim6@gmail.com>
2025-09-29 18:20:49 +0600

    notification api

--- controllers/notificationController.js
+++ controllers/notificationController.js
@@ -3,6 +3,9 @@ import client from "../config/database.js";
 const msgCollection = client.db("shajidint").collection("Msgs");
 
 export const getNotification = async (req, res) => {
-  const notification = await msgCollection.find().toArray();
+  const notification = await msgCollection
+    .find()
+    .sort({ sendDateFormatted: -1 })
+    .toArray();
   res.send(notification);
 };
